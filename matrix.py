8eb167425b4c4a48f34fe73328ee543403ee281d
Charles Pascoe <cpascoe95@gmail.com>
2015-06-03 09:34:52 +0100

    Removed unnecessary code

--- matrix.py
+++ matrix.py
@@ -83,11 +83,3 @@ class Matrix:
 
         return Matrix(m)
 
-
-if __name__ == '__main__':
-    m1 = Matrix([[1, 2, 3], [4, 5, 6]])
-    m2 = Matrix([[7, 8], [9, 10], [11, 12]])
-
-    m3 = m1 * m2
-
-    print(m3.get_matrix())
