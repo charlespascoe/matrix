c071547ce47b79a109bc63f969ef7877baf8ffdc
Charles Pascoe <cpascoe95@gmail.com>
2015-06-09 17:10:19 +0100

    Moved Arithmetic magic methods and Class methods to the end of the class

--- matrix.py
+++ matrix.py
@@ -10,13 +10,11 @@ class Matrix:
         self._columns = len(matrix[0]) if len(matrix) > 0 else 0
         self._m = matrix
 
-    @classmethod
-    def empty_matrix(cls, rows, columns):
-        return cls([[0 for j in range(columns)] for i in range(rows)])
+    def __getitem__(self, index):
+        return self._m[index]
 
-    @classmethod
-    def identity_matrix(cls, size):
-        return cls([[int(i == j) for j in range(size)] for i in range(size)])
+    def __str__(self):
+        return '{}x{}'.format(self.rows, self.columns)
 
     @property
     def rows(self):
@@ -26,17 +24,83 @@ class Matrix:
     def columns(self):
         return self._columns
 
-    def __getitem__(self, index):
-        if index < 0 or index >= len(self._m):
-            raise Exception()
-
-        return self._m[index]
-
     def get_matrix(self):
         return copy.deepcopy(self._m)
 
-    def __str__(self):
-        return '{}x{}'.format(self.rows, self.columns)
+    def submatrix(self, not_i, not_j):
+        return Matrix([[self._m[i][j] for j in range(self.columns) if j != not_j] for i in range(self.rows) if i != not_i])
+
+    def determinant(self):
+        if self.rows != self.columns or self.rows == 0:
+            raise MatrixArithmeticError('Determinant can only be calculated for square matrices')
+
+        if self.rows == 1:
+            return self[0][0]
+
+        if self.rows == 2:
+            return self[0][0] * self[1][1] - self[0][1] * self[1][0]
+
+        coefficient = 1
+
+        det = 0
+
+        for j in range(self.columns):
+            det += coefficient * self[0][j] * self.submatrix(0, j).determinant()
+            coefficient *= -1
+
+        return det
+
+    def cofactor_matrix(self):
+        if self.rows != self.columns or self.rows == 0:
+            raise MatrixArithmeticError('Cofactor matrix can only be calculated for square matrices')
+
+        if self.rows == 1:
+            return Matrix([[self[0][0]]])
+
+        c = Matrix.empty_matrix(self.rows, self.columns)
+
+        for i in range(self.rows):
+            for j in range(self.columns):
+                c[i][j] = pow(-1, i+j) * self.submatrix(i, j).determinant()
+
+        return c
+
+    def transpose(self):
+        return Matrix([[self[i][j] for i in range(self.rows)] for j in range(self.columns)])
+
+    def inverse(self):
+        det = self.determinant()
+
+        if det == 0:
+            return None
+
+        return self.cofactor_matrix().transpose() / det
+
+    def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError('Matrix addition requires another matrix')
+
+        if self.rows != other.rows or self.columns != other.columns:
+            raise MatrixArithmeticError('Cannot add - invalid matrix sizes')
+
+        m = [[0 for j in range(self.columns)] for i in range(self.rows)]
+
+        for i in range(self.rows):
+            for j in range(self.columns):
+                m[i][j] = self[i][j] + other[i][j]
+
+        return Matrix(m)
+
+    def __mod__(self, other):
+        if not isinstance(other, int) or isinstance(other, float):
+            raise TypeError('Invalid modulus type: {}'.format(type(other)))
+
+        m = self.get_matrix()
+        for i in range(self.rows):
+            for j in range(self.columns):
+                m[i][j] %= other
+
+        return Matrix(m)
 
     def __mul__(self, other):
         if isinstance(other, int) or isinstance(other, float):
@@ -99,79 +163,10 @@ class Matrix:
 
         return result
 
-    def __add__(self, other):
-        if not isinstance(other, Matrix):
-            raise TypeError('Matrix addition requires another matrix')
+    @classmethod
+    def empty_matrix(cls, rows, columns):
+        return cls([[0 for j in range(columns)] for i in range(rows)])
 
-        if self.rows != other.rows or self.columns != other.columns:
-            raise MatrixArithmeticError('Cannot add - invalid matrix sizes')
-
-        m = [[0 for j in range(self.columns)] for i in range(self.rows)]
-
-        for i in range(self.rows):
-            for j in range(self.columns):
-                m[i][j] = self[i][j] + other[i][j]
-
-        return Matrix(m)
-
-    def __mod__(self, other):
-        if not isinstance(other, int) or isinstance(other, float):
-            raise TypeError('Invalid modulus type: {}'.format(type(other)))
-
-        m = self.get_matrix()
-        for i in range(self.rows):
-            for j in range(self.columns):
-                m[i][j] %= other
-
-        return Matrix(m)
-
-    def submatrix(self, not_i, not_j):
-        return Matrix([[self._m[i][j] for j in range(self.columns) if j != not_j] for i in range(self.rows) if i != not_i])
-
-    def determinant(self):
-        if self.rows != self.columns or self.rows == 0:
-            raise MatrixArithmeticError('Determinant can only be calculated for square matrices')
-
-        if self.rows == 1:
-            return self[0][0]
-
-        if self.rows == 2:
-            return self[0][0] * self[1][1] - self[0][1] * self[1][0]
-
-        coefficient = 1
-
-        det = 0
-
-        for j in range(self.columns):
-            det += coefficient * self[0][j] * self.submatrix(0, j).determinant()
-            coefficient *= -1
-
-        return det
-
-    def cofactor_matrix(self):
-        if self.rows != self.columns or self.rows == 0:
-            raise MatrixArithmeticError('Cofactor matrix can only be calculated for square matrices')
-
-        if self.rows == 1:
-            return Matrix([[self[0][0]]])
-
-        c = Matrix.empty_matrix(self.rows, self.columns)
-
-        for i in range(self.rows):
-            for j in range(self.columns):
-                c[i][j] = pow(-1, i+j) * self.submatrix(i, j).determinant()
-
-        return c
-
-    def transpose(self):
-        return Matrix([[self[i][j] for i in range(self.rows)] for j in range(self.columns)])
-
-    def inverse(self):
-        det = self.determinant()
-
-        if det == 0:
-            return None
-
-        return self.cofactor_matrix().transpose() / det
-
-
+    @classmethod
+    def identity_matrix(cls, size):
+        return cls([[int(i == j) for j in range(size)] for i in range(size)])
